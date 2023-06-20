75500186b6764fac3c31504298a2d7a7994cd04a
Viana Ferguson <vianaferguson@yahoo.com>
2023-06-20 00:14:43 -0500

    edit booking-submit.php

--- book/booking-submit.php
+++ book/booking-submit.php
@@ -1,25 +1,14 @@
 <?php
-// Retrieve form data
-$name = $_POST['name'];
-$email = $_POST['email'];
-$subject = $_POST['subject'];
-$message = $_POST['message'];
+    if(isset($_POST['submit'])) { 
+        $name = $_POST['name'];
+        $subject = $_POST['subject'];
+        $mailFrom = $_POST['email'];
+        $message = $_POST['message'];
 
-// Validate and sanitize inputs (additional validation can be added)
-$name = filter_var($name, FILTER_SANITIZE_STRING);
-$email = filter_var($email, FILTER_SANITIZE_EMAIL);
-$subject = filter_var($subject, FILTER_SANITIZE_STRING);
-$message = filter_var($message, FILTER_SANITIZE_STRING);
+        $mailTo = "booking@colormehigh.com";
+        $headers = "From: ".$mailFrom;
+        $txt = "You have an email from ".$name.".\r\n".$mailFrom.".\r\n About ".$subject.".\r\n".$message;
 
-// Compose email message
-$to = 'your-email@example.com';
-$headers = "From: $name <$email>" . "\r\n";
-$message = "Subject: $subject\n\n$message";
-
-// Send email
-if (mail($to, $subject, $message, $headers)) {
-  echo 'Message sent successfully.';
-} else {
-  echo 'Error sending message.';
-}
-?>
+        mail($mailTo, $subject, $txt, $headers);
+        header("Location: booking.html?mailsend");
+    }  
